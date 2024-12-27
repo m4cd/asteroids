ae70b14db4ee695e769ee500bf4adbf8291a6383
m4cd <maciej.ziebicki@outlook.com>
2024-12-27 09:04:02 +0100

    FPS

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from constants import *
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
         for event in pygame.event.get():
@@ -11,6 +13,8 @@ def main():
                 return
         screen.fill(color=(0,0,0))
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
+        print(dt)
 
 
 if __name__ == "__main__":
